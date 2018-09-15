2273721899839a2605e8bb3d895701680469e3d4
Scratch <Scrxtchy@users.noreply.github.com>
2018-09-15 19:51:00 +1000

    Update vsqxmidi.py

--- vsqxmidi.py
+++ vsqxmidi.py
@@ -2,23 +2,26 @@ from midiutil import MIDIFile
 
 import xml.dom.minidom
 import sys
+from pathlib import PurePath
 
 def getNoteData(note, key):
 	return int(note.getElementsByTagName(key)[0].firstChild.data)
 
-vsqx = xml.dom.minidom.parse(sys.argv[1])
+path = PurePath(sys.argv[1])
+
+vsqx = xml.dom.minidom.parse(str(path))
 
 TEMPO = int(vsqx.getElementsByTagName('tempo')[0].childNodes[1].firstChild.data[:-2])
 
-mf = MIDIFile(1, removeDuplicates=False)
-track = 0
+mf = MIDIFile(len(vsqx.getElementsByTagName('vsTrack')), removeDuplicates=False)
 
 time = 0
-mf.addTrackName(track, time, "Sample")
-mf.addTempo(track, time, TEMPO)
 
-for note in vsqx.getElementsByTagName('note'):
-	mf.addNote(track, 0, getNoteData(note, 'n'), getNoteData(note, 't') / 480, getNoteData(note, 'dur') / 480, getNoteData(note, 'v'))
+for trackNo, track in enumerate(vsqx.getElementsByTagName('vsTrack')):
+	mf.addTrackName(trackNo, time, "Track {}".format(str(trackNo)))
+	for note in track.getElementsByTagName('note'):
+		mf.addNote(trackNo, 0, getNoteData(note, 'n'), getNoteData(note, 't') / 480, getNoteData(note, 'dur') / 480, getNoteData(note, 'v'))
+	mf.addTempo(trackNo, time, TEMPO)
 
-with open("vsqxmidi.mid", 'wb') as outf:
+with open(str(path.parents[0]) +'\\'+ path.stem + ".mid", 'wb') as outf:
 	mf.writeFile(outf)
